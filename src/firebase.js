1d7690ae3991bd01cef8cd8828cfaf89f95ab976
Koji Mochizuki <kjmczk@minim.local>
2019-09-06 13:01:32 +0900

    Fix equals operators

--- src/firebase.js
+++ src/firebase.js
@@ -21,11 +21,11 @@ firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
 
 db.enablePersistence().catch(err => {
-  if (err.code == 'failed-precondition') {
+  if (err.code === 'failed-precondition') {
     // Multiple tabs open, persistence can only be enabled
     // in one tab at a a time.
     console.log('persistance failed');
-  } else if (err.code == 'unimplemented') {
+  } else if (err.code === 'unimplemented') {
     // The current browser does not support all of the
     // features required to enable persistence
     console.log('persistance not available');
