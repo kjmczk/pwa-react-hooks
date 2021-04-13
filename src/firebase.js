a2a518d480ebbca08c0d6cec0a13a8271533b809
Koji Mochizuki <vollgong@gmail.com>
2021-04-13 14:09:35 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
 
 // Add the Firebase products that you want to use
 import 'firebase/firestore';
